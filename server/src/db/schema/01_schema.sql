6f178192c365d4422a4b48ce98bb2050883c3c39
gurpreet <gurpreet212004@gmail.com>
2021-09-25 16:04:32 -0400

    add schema and seeds for db

--- server/src/db/schema/01_schema.sql
+++ server/src/db/schema/01_schema.sql
@@ -1,17 +1,71 @@
+-- DROP TABLE IF EXISTS users CASCADE;
+-- DROP TABLE IF EXISTS messages CASCADE;
+-- CREATE TABLE users(
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   name VARCHAR(255) NOT NULL
+-- );
+
+-- CREATE TABLE messages(
+
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   text VARCHAR(1000) NOT NULL,
+--   date VARCHAR(1000) NOT NULL,
+--   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+-- );
+
 DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS playlists CASCADE;
+DROP TABLE IF EXISTS users_playlists CASCADE;
+DROP TABLE IF EXISTS media CASCADE;
+DROP TABLE IF EXISTS playlists_media CASCADE;
 DROP TABLE IF EXISTS messages CASCADE;
 
 
 CREATE TABLE users(
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  avatar VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE playlists(
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  rating INT,
+  thumbnail VARCHAR(255),
+  queue BOOLEAN 
+);
+
+CREATE TABLE users_playlists(
+  id SERIAL PRIMARY KEY NOT NULL,
+  is_host BOOLEAN,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  playlist_id INTEGER REFERENCES playlists(id) ON DELETE CASCADE
+);
+
+CREATE TABLE media(
+  id SERIAL PRIMARY KEY NOT NULL,
+  link VARCHAR(255) NOT NULL,
+  category VARCHAR(255) NOT NULL,
+  thumbnail VARCHAR(255),
+  description TEXT
+);
+
+CREATE TABLE playlists_media(
+  id SERIAL PRIMARY KEY NOT NULL,
+  media_rating INTEGER,
+  play_order Integer,
+  played_already BOOLEAN,
+  played_length FLOAT,
+  media_id INTEGER REFERENCES media(id) ON DELETE CASCADE,
+  playlist_id INTEGER REFERENCES playlists(id) ON DELETE CASCADE
 );
 
 CREATE TABLE messages(
-
   id SERIAL PRIMARY KEY NOT NULL,
-  text VARCHAR(1000) NOT NULL,
-  date VARCHAR(1000) NOT NULL,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
-
+  text VARCHAR(255),
+  date VARCHAR(255),
+  users_playlists_id INTEGER REFERENCES users_playlists(id) ON DELETE CASCADE
 );
+
--- server/src/db/seeds/01_seeds.sql
+++ server/src/db/seeds/01_seeds.sql
@@ -1,13 +1,77 @@
-INSERT INTO users (name)
-  VALUES
-  ('Irene'),
-  ('Georgie'),
-  ('Betty'),
-  ('Mayme');
+-- INSERT INTO users (name)
+--   VALUES
+--   ('Irene'),
+--   ('Georgie'),
+--   ('Betty'),
+--   ('Mayme');
   
-INSERT INTO messages(text, date, user_id)
-  VALUES
-  ('Hello from Irene!',   1632010842289, 1),
-  ('Hello from Georgie',  1632010841481, 2),
-  ('Hello from Betty',    1632010842289, 3),
-  ('Hello from Mayme',    1632010843017, 4);
+-- INSERT INTO messages(text, date, user_id)
+--   VALUES
+--   ('Hello from Irene!',   1632010842289, 1),
+--   ('Hello from Georgie',  1632010841481, 2),
+--   ('Hello from Betty',    1632010842289, 3),
+--   ('Hello from Mayme',    1632010843017, 4);
+
+INSERT INTO users (id, username, email, password, avatar)
+            VALUES (1, 'Alpha', 'alpha@gmail.com', '123', 'https://static.toiimg.com/thumb/resizemode-4,msid-76729750,imgsize-249247,width-720,height-540/76729750.jpg'),
+            (2, 'Beta', 'beta@gmail.com', '123', 'https://www.w3schools.com/howto/img_avatar2.png'),
+            (3, 'Charlie', 'charlie@gmail.com', '123','https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes-thumbnail.png'),
+            (4, 'Delta', 'delta@gmail.com', '123','https://www.nj.com/resizer/h8MrN0-Nw5dB5FOmMVGMmfVKFJo=/450x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg');
+
+INSERT INTO playlists(id, name , rating, thumbnail, queue)
+            VALUES(1, 'PL-1', 4, 'https://assets.smoothradio.com/2017/26/ed-sheeran-1498662806-editorial-short-form-0.jpg', true),
+            (2, 'PL-2', 5,'https://media1.popsugar-assets.com/files/thumbor/wNhs3Orm_ob0T6fsFhqfRgMRCEw/1346x474:4298x3426/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2019/10/31/972/n/1922283/b80874af5dbb5e357a48b0.76757692_/i/Best-Young-Music-Artists-2019.jpg' , false),
+            (3, 'PL-3', 4,'https://www.grammy.com/sites/com/files/styles/news_detail_header/public/gettyimages-1014868608.jpg?itok=eLyDCDbM' , false),
+            (4, 'PL-4', 5,'https://i.insider.com/5b21452f1ae6623b008b5306?width=750&format=jpeg&auto=webp' , false),
+            (5, 'PL-5', 4,'https://www.liveabout.com/thmb/CXNryNO4dxhyT13v41hVmKXHq1Q=/3000x2000/filters:no_upscale():max_bytes(150000):strip_icc()/weeknd-56a96cf75f9b58b7d0fb536c.jpg' , false);
+
+INSERT INTO media (id, link, category, thumbnail, description)
+            VALUES(1, 'https://www.youtube.com/watch?v=9bZkp7q19f0&list=PL15B1E77BB5708555&index=1', 'video', 't', 'desc1'),
+            (2, 'https://www.youtube.com/watch?v=OPf0YbXqDm0&list=PL15B1E77BB5708555&index=2', 'video', 't', 'desc1'),
+            (3, 'https://www.youtube.com/watch?v=OPf0YbXqDm0&list=PL15B1E77BB5708555&index=3', 'video', 't', 'desc1'),
+            (4, 'https://www.youtube.com/watch?v=OPf0YbXqDm0&list=PL15B1E77BB5708555&index=4', 'video', 't', 'desc1'),
+            (5, 'https://www.youtube.com/watch?v=OPf0YbXqDm0&list=PL15B1E77BB5708555&index=5', 'video', 't', 'desc1'),
+            (6, 'https://www.youtube.com/watch?v=OPf0YbXqDm0&list=PL15B1E77BB5708555&index=6', 'video', 't', 'desc1'),
+            (7, 'https://www.youtube.com/watch?v=OPf0YbXqDm0&list=PL15B1E77BB5708555&index=7', 'video', 't', 'desc1'),
+            (8, 'https://www.youtube.com/watch?v=OPf0YbXqDm0&list=PL15B1E77BB5708555&index=8', 'video', 't', 'desc1'),
+            (9, 'https://www.youtube.com/watch?v=kJQP7kiw5Fk&list=PL15B1E77BB5708555&index=9', 'video', 't', 'desc1'),
+            (10, 'https://www.youtube.com/watch?v=JGwWNGJdvx8&list=PL15B1E77BB5708555&index=10', 'video', 't', 'desc1');
+
+INSERT INTO users_playlists (id, is_host, user_id, playlist_id)
+            VALUES(1,true, 1 , 1),
+            (2,false, 2 , 1),
+            (3,false, 3 , 1),
+            (4,false, 4 , 1),
+            (5,false, 1 , 2),
+            (6,true, 2 , 2),
+            (7,false, 3 , 2),
+            (8,false, 4 , 2),
+            (9,false, 1 , 3),
+            (10,false, 2 , 3);
+
+INSERT INTO playlists_media (id, media_rating, play_order, played_already, played_length, media_id, playlist_id)
+            VALUES (1, 3, 1, true, 1.10, 1, 1),
+            (2, 4, 2, false, 0.00, 2, 1),
+            (3, 1, 3, false, 0.00, 3, 1),
+            (4, 6, 4, false, 0.00, 4, 1),
+            (5, 2, 1, true, 2.00, 2, 2),
+            (6, 3, 2, true, 2.00, 3, 2),
+            (7, 6, 3, false, 0.00, 5, 2),
+            (8, 4, 4, false, 0.00, 6, 2),
+            (9, 1, 1, false, 0.00, 6, 3),
+            (10, 5, 2, false, 0.00, 7, 3),
+            (11, 3, 3, false, 0.00, 8, 3),
+            (12, 3, 1, true, 1.20, 9, 4),
+            (13, 2, 2, false, 0.00, 3, 4),
+            (14, 1, 3, false, 0.00, 10, 4),
+            (15, 3, 1, false, 0.00, 4, 5);
+
+INSERT INTO messages (id, text, date, users_playlists_id)
+            VALUES(1, 'Hello There !!',1632010842289,1),
+            (2, 'Hi !!',1632010842289,2),
+            (3, 'Whats up..',1632010842289,1),
+            (4, 'All good',1632010842289,3),
+            (5, 'new or old playlist',1632010842289,4),
+            (6, 'old will be good',1632010842289,2),
+            (7, 'great!!',1632010842289,1);
+            
