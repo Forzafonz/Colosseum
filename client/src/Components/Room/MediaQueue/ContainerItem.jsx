581d5653e751fbce7c5fd71d4fba8e705be91bba
Alex MacLure <almaclure@gmail.com>
2021-09-29 15:24:56 -0400

    Add image and description to queue item

--- client/src/Components/Room/MediaQueue/ContainerItem.jsx
+++ client/src/Components/Room/MediaQueue/ContainerItem.jsx
@@ -1,49 +1,31 @@
 import React from 'react'
-import { useContext } from 'react'
-import { UserContext } from '../../../hooks/userContext'
 import './ContainerItem.scss'
 
 const ContainerItem = React.forwardRef((props, ref) => {
-const {id} = props;
-const { state, dispatch }  = useContext(UserContext);
-const formRef = React.createRef();
 
-const clickHandler = (e) => {
-  e.preventDefault()
-  dispatch({type: "modify", values: {msg: formRef.current.value, 'id' : id}})
-  formRef.current.value = "";
-}
+  const {id, dispatch, play_order, thumbnail, description, media_rating} = props;
 
-const addLike = () => {
-  dispatch({ type: "LIKE", values : {id}})
-}
+  const addLike = () => {
+    dispatch({ type: "LIKE", values : {id}})
+  }
 
-const handleDragStart = (event, id) => {
-  const draggedItem = state[id];
-  console.log(draggedItem)
-  event.dataTransfer.effectAllowed = "move";
-  event.dataTransfer.setData("text/plain", event.target.parentNode);
-  console.log(event.target.parentNode)
-}
+  return (
+    <div
+      id = {id} className = "main-block" ref = {ref}>
+      <div className = "remove-button" onClick = {() => dispatch({ type: "remove" , values: {id}})}>✕</div>
+      <span className = "likes-dislikes">
+        <div className = "social-button social-likes" onClick = {() => addLike()}>👍  {media_rating}/10</div>
+      </span>
+      <div>
+        {description}
+      </div>
+      <div>
+        <img className = "img-thumbnail" src = {thumbnail}/>
+      </div>
+      
 
-const handleOnDrop = (event) => {
-  const id  = event.dataTransfer.getData("text/plain");
-  console.log(event.target.parentNode)
-  console.log("ID", id)
-}
-
-return (
-  <div 
-    onDragStart = {(event) => handleDragStart(event, id)} 
-    onDrop = {(event) => handleOnDrop(event)}
-    id = {id} draggable className = "main-block" ref = {ref}>
-    <div className = "remove-button" onClick = {() => dispatch({ type: "remove" , values: {id}})}>✕</div>
-    <span >{state[id].msg ? state[id].msg : "No message typed!"}</span>
-    <span className = "likes-dislikes">
-      <div className = "social-button social-likes" onClick = {() => addLike()}>👍  {state[id].votes}</div>
-    </span>
-  </div>
-)
+    </div>
+  )
 });
 
 export default ContainerItem;
