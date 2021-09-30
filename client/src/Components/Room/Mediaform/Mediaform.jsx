5857f3fb153dad5c106f29883f16a131f26337ff
Anton <golubin1990@gmail.com>
2021-09-30 18:19:36 -0400

    Add video add functionality

--- client/src/Components/Room/Mediaform/Mediaform.jsx
+++ client/src/Components/Room/Mediaform/Mediaform.jsx
@@ -12,6 +12,9 @@ function Mediaform() {
   const [category, setCategory] = useState('youtube'); //youtube or soundcloud
   const [desc, setDesc] = useState(''); //Description for media
   const [buttonLabel, setButtonLabel] = useState('Search')
+  // State to keep search result to display them in SearchResultsItem
+  const [searchResults, setSearchResults] = useState({})
+
   // UPDATE INITIAL STATE ONCE FIXED CURRENT INITIAL STATE FOR TEST ONLY
   const [playlistName, setPlaylistName] = useState("Lolo"); 
   // UPDATE INITIAL STATE ONCE FIXED CURRENT INITIAL STATE FOR TEST ONLY
@@ -33,20 +36,27 @@ function Mediaform() {
 //To add media to playlist
   const addMedia = () => {
 
-    const playlist_id = currentplaylist_id;
-    const data = {
-      url,
-      category,
-      playlist_id,
-      desc,
-    };
-
-     if (!url.includes("http")) {
-       console.log("THIS IS URL Search:", url)
+     if (!url.includes("http") && category === "youtube") {
         axios.get(`/api/youtube/${url}`). then((result) => {
-            console.log(result)
+          const newResults = {};
+          result.data.forEach((result, index) =>{
+            const {id, snippet} = result;
+            const {thumbnails, title, description} = snippet;
+            newResults[index] ={
+              link:`https://www.youtube.com/watch?v=${id.videoId}`,
+              thumbnail : thumbnails.default,
+              title,
+              description
+            }
+          })
+          setSearchResults(newResults);
         })
         .catch((error) => console.error("This is error", error))
+     } else if (!url.includes("http") && category === "soundcloud") {
+        //  do something
+     } else {
+        
+        submitMedia({url, desc})
      }
 
     // axios.put('http://localhost:8000/api/addmedia', { data }).then((res) => {
@@ -55,6 +65,26 @@ function Mediaform() {
     //   setDesc('');
     // });
   };
+
+  const submitMedia = ({url, desc, image}) => {
+
+    const playlist_id = currentplaylist_id;
+    const data = {
+      url,
+      category,
+      playlist_id,
+      desc,
+      thumbnail : image
+    };
+
+    console.log("I am submitting with this data:", data)
+      axios.put('http://localhost:8000/api/addmedia', { data }).then((res) => {
+      alert('Playlist updated');
+      setUrl('');
+      setDesc('');
+    });
+
+  }
 
   const newSearch = (e) =>{
     if (e.target.value.includes("http")){
@@ -69,8 +99,8 @@ function Mediaform() {
     <div className="new-p-container">
       <br/>      
 
-      <h1 className ="h-pl">Playlist </h1>
-      <h1 className ="h-name">{playlistName} </h1>
+      <h1 className ="h-pl">Playlist : {playlistName} </h1>
+
       <br/>
 
 
@@ -144,10 +174,49 @@ function Mediaform() {
       </table>
 
       <section className = "search-results-container">
-           <SearchResultsContainer /> 
+           <SearchResultsContainer
+            searchResults = {searchResults}
+            submitMedia = {submitMedia}
+            /> 
       </section>
     </div>
   );
 }
 
 export default Mediaform;
+
+
+
+// YOUTUBE RESPONSE:
+// array of objects:[{}, {}, {}]
+// 0: 
+//  {etag: "xWucDJa1s76RtOIFkiX_Fv5yCl8"
+// id:
+//  {kind: "youtube#video"
+// videoId: "kXYiU_JCYtU"
+// [[Prototype]]: Object}
+// kind: "youtube#searchResult"
+// snippet:{
+// channelId: "UCZU9T1ceaOgwfLRq7OKFU4Q"
+// channelTitle: "Linkin Park"
+// description: "Watch the official music video for Numb by Linkin Park from the album Meteora. Stream: https://apple.co/2OCBU1k Subscribe to the channel: ..."
+// liveBroadcastContent: "none"
+// publishTime: "2007-03-05T08:12:00Z"
+// publishedAt: "2007-03-05T08:12:00Z"
+// thumbnails: {default: {…}, medium: {…}, high: {…}}
+// title: "Numb [Official Music Video] - Linkin Park"}
+//1:
+// {etag: "S2hC7LPUC8JTcWWIwqg_LZ-QGEg"
+// id:
+// {kind: "youtube#video"
+// videoId: "xGvIdbB67Qs"
+// [[Prototype]]: Object}
+// kind: "youtube#searchResult"
+// snippet:{
+// channelId: "UC0fGycccXWcPvOWgjKPO8dQ"
+// channelTitle: "Jorge Gonzalez"
+// description: "01 One step closer 00:00 02 Lost in the echo 02:35 03 Somewhere I belong 06:01 04 Given up 09:35 05 Papercut 12:44 06 Waiting for the end 15:50 07 Burn it ..."
+// liveBroadcastContent: "none"
+// publishTime: "2020-02-01T15:42:59Z"
+// publishedAt: "2020-02-01T15:42:59Z"
+// thumbnails: {default: {…}, medium: {…}, high: {…}}}
--- client/src/Components/Room/Mediaform/SearchResultsContainer.jsx
+++ client/src/Components/Room/Mediaform/SearchResultsContainer.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import {Navbar, Container, Row, Col} from 'react-bootstrap';
 import SearchResultsItem from "./SearchResultsItem"
 
-export default function SearchResultsContainer() {
+export default function SearchResultsContainer({searchResults, submitMedia}) {
+  console.log("THIS IS SEACRH RESULTS:", searchResults)
+
+  const results = Object.keys(searchResults).map(resultKey => {
+
+      return (
+        <Row key = {resultKey}>
+            <Col className ="d-flex justify-content-center">
+              <SearchResultsItem 
+              {...searchResults[resultKey]}
+              submitMedia = {submitMedia}
+              />
+            </Col>
+        </Row>
+      )
+
+  })
+
   return (
-    <div>
-      <SearchResultsItem />
-    </div>
+  <Container fluid="md">
+    {results}
+  </Container>
   )
 }
